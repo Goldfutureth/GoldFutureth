dd79806e750bc3aa624fbc183d6d5b6c7fb759a5
GoldFutureth <you@example.com>
2025-04-16 00:34:26 +0700

    chore: downgrade tailwindcss to v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
+// app/layout.tsx
 import './globals.css'
+import type { Metadata } from 'next'
 
-
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Gold&Future Academy',
   description: 'เทรดอย่างมืออาชีพ ก้าวสู่อนาคตที่มั่งคั่ง',
-};
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="th">
+      <head />
       <body>{children}</body>
     </html>
-  );
+  )
 }
